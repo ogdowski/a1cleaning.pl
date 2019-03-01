75fd2f9198643f6ad8fbe64ae39003fbf8d3478b
Ariel Ogdowski <ogdowski@protonmail.com>
2019-03-01 18:30:06 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+function main() {
+
+(function () {
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+
+    $('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+          }
+        }
+      });
+
+   'use strict';
+
+    $(window).bind('scroll', function() {
+        var navHeight = 200;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    })
+
+  	$(document).ready(function() {
+
+  	});
+
+}());
+
+
+}
+main();
